498cb1a3b0d9eda574f3b0950c84ed12089e280a
jhocelinne vazquez <jhocelinne.vazquez@uabc.edu.mx>
2023-02-19 19:20:39 -0800

    hola b

---
+++ clase.java
@@ -0,0 +1,11 @@
+  GNU nano 7.1                       clase.java                       Modified
+public static void main(String[] args) {
+        String name="Jhocelinne";
+    System.out.println("Hola mundo, este repositorio fue creado por:"+name);
+  }
+}
+
+
+
+
+
